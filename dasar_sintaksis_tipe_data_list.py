1783e2ed242408478bb13f1484a48d5b066486d8
ssd <ifanmolanalearn@gmail.com>
2023-07-13 20:42:56 +0700

    Memahami dan menggunakan list part 2

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -25,3 +25,34 @@ print('Tambahkan 1 buku baru')
 daftar_buku.append('Dunia Matematik Kelas 5')
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nClear List')
+daftar_buku.clear()
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti elemen pertama')
+daftar_buku = ['Seven Habits', 'How to influence People', 'First Things First', '4DX']
+daftar_buku[0] = 'Eight Habits'
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil elemen ke-2')
+buku = daftar_buku.pop(1)
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop')
+daftar_buku.pop()
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPop -1')
+daftar_buku = ['Seven Habits', 'How to influence People', 'First Things First', '4DX']
+daftar_buku.pop(-4)
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
