4a1c43f41421993c96e068028863a2a7cdcf52b1
ssd <ifanmolanalearn@gmail.com>
2023-07-20 10:25:31 +0700

    Dasar sintaksis dictionary menggunakan data users format JSON

---
+++ dasar_sintaksis_dict.py
@@ -0,0 +1,11 @@
+users = {
+    'id' : '1',
+    'name' : 'Leanne Garaham',
+    'username' : 'Bret',
+    'email' : 'Sincere@april.biz',
+}
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
