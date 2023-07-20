21ae903cc23e00593c3953ad4d7e1fd2186ca3ff
ssd <ifanmolanalearn@gmail.com>
2023-07-20 10:46:55 +0700

    Dasar Sintaksis dictionary dalam dictionary

--- dasar_sintaksis_dict.py
+++ dasar_sintaksis_dict.py
@@ -1,11 +1,26 @@
 users = {
-    'id' : '1',
-    'name' : 'Leanne Garaham',
-    'username' : 'Bret',
-    'email' : 'Sincere@april.biz',
+    "id" : "1",
+    "name" : "Leanne Garaham",
+    "username" : "Bret",
+    "email" : "Sincere@april.biz",
+    "address" : {
+        "street" : "Kulas light",
+        "suite" : "Apt. 556",
+        "city" : "Gwenborough",
+        "zipcode" : "92998-3874",
+        "geo" : {
+            "lat" : "-37.3159",
+            "lng" : "81.1496"
+        }
+    }
 }
 print(users)
-print(users['id'])
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"])
+print(users["address"]["street"])
+print(users["address"]["geo"])
+print(users["address"]["geo"]["lat"])
+print(users["address"]["geo"]["lng"])
